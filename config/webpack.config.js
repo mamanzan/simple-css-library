dee27203b899de317bba8246f5231f43c3a4944b
Michael A Manzano <michael.a.manzano@intel.com>
2022-04-08 16:42:53 -0700

    use scss

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,10 +2,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.s[ac]ss$/i,
         use: [
           'style-loader',
-          'css-loader'
+          'css-loader',
+          'sass-loader'
         ]
       }
     ]
